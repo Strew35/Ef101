8422fa81699d9d7b57b67851a02878de889ba9d9
strew35 <73694058+Strew35@users.noreply.github.com>
2022-12-17 10:16:36 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    /// <summary>
+    /// T=>Entity
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
